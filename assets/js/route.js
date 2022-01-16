583c399df8a9246cc9b3e4a0dd2bb6126cf8a725
Sean <seanpatmeade@gmail.com>
2022-01-16 18:40:40 +0000

    Collect coords of click as array and add layer for starting point with place holder (empty) GeoJSON.

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,6 +1,9 @@
 // API Key
 mapboxgl.accessToken = "pk.eyJ1Ijoic2Vhbi1tZWFkZSIsImEiOiJja3lmeDZkM3Iwc21hMm9xcG95YnFqaHh3In0.p4oU6PP7a92U1JYLBCLG2g"
 
+// an empty geoJSON feature collection for place holder
+var placeholder = turf.featureCollection([]);
+
 // Initialize a map with the center being an view of Ireland
 const map = new mapboxgl.Map({
   container: 'map',
@@ -26,18 +29,46 @@ geocoder.on("result", (e) => {
   map.setCenter(e.result.center);
 });
 
-function successLocation(e) {
+// set center of map to current location if success
+function successCurrentLocation(e) {
   // console.log([e.coords.longitude, e.coords.latitude]);
   map.setCenter([e.coords.longitude, e.coords.latitude]);
 }
 
-function errorLocation(e) {
+// If current location can't be found send alert
+function errorCurrentLocation(e) {
   alert("Couldn't find your current location");
 }
 
+// Function called when button is pressed for using current location
 function useCurrentLocation(e) {
-  navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  navigator.geolocation.getCurrentPosition(successCurrentLocation, errorCurrentLocation, {
     enableHighAccuracy: true,
   });
-  
 }
+
+// When map is clicked collect lat and lng
+map.on('click', function (e) {
+  var coords = e.lngLat;
+  var click = [coords.lng, coords.lat];
+  console.log(click);
+}); // map on click
+
+// Set first click or current location with starting point symbol
+// Create layer for it when map has loaded:
+map.on('load', function() {
+  map.addLayer({
+    id: 'currentLoc',
+    type: 'circle',
+    source: {
+      data: placeholder,
+      type: 'geojson',
+    },
+    paint: {
+      'circle-radius': 10,
+      'circle-color': 'white',
+      'circle-stroke-color': '#3887be',
+      'circle-stroke-width': 3,
+    },
+  });
+})
