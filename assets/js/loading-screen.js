4fd141b2f8ba18bde85f6c69e2fc19458dfd0281
Sean <seanpatmeade@gmail.com>
2022-01-22 19:52:43 +0000

    Seperate output and input in route-settings.

--- assets/js/loading-screen.js
+++ assets/js/loading-screen.js
@@ -5,21 +5,27 @@ const bg = document.querySelector('.bg');
 
 let load = 0;
 
-let int = setInterval(blurring, 30);
+// let int = setInterval(blurring, 30);
 
-function blurring() {
-  load++
+// function blurring() {
+//   load++
 
-  if (load > 99) {
-    clearInterval(int);
-    loadText.style.display = "none";
-    bg.style.display = "none";
-    document.body.style.overflow = "visible";
-  }
+//   if (load > 99) {
+//     clearInterval(int);
+//     loadText.style.display = "none";
+//     bg.style.display = "none";
+//     document.body.style.overflow = "visible";
+//   }
 
-  console.log((1 - load/100).toFixed(2));
+//   console.log((1 - load/100).toFixed(2));
 
-  loadText.innerText = `${load}%`;
-  loadText.style.opacity = (1 - load/100);
-  bg.style.filter = `blur(${Math.round(load/3)}px)`;
-}
+//   loadText.innerText = `${load}%`;
+//   loadText.style.opacity = (1 - load/100);
+//   bg.style.filter = `blur(${Math.round(load/3)}px)`;
+// }
+
+
+// delete these and uncomment above before final submit
+loadText.style.display = "none";
+bg.style.display = "none";
+document.body.style.overflow = "visible";
