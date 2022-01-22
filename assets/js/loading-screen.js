c45f061b1cc7d454d4691bc8ae579ff08bd7ca89
Sean <seanpatmeade@gmail.com>
2022-01-22 18:26:43 +0000

    Add blur of loading image and when loaded set both divs involved in loading screen to display = none and set overflow of body back to visible.

--- assets/js/loading-screen.js
+++ assets/js/loading-screen.js
@@ -1,3 +1,5 @@
+// https://www.udemy.com/course/50-projects-50-days/
+
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
 
@@ -10,10 +12,14 @@ function blurring() {
 
   if (load > 99) {
     clearInterval(int);
+    loadText.style.display = "none";
+    bg.style.display = "none";
+    document.body.style.overflow = "visible";
   }
 
   console.log((1 - load/100).toFixed(2));
 
   loadText.innerText = `${load}%`;
   loadText.style.opacity = (1 - load/100);
+  bg.style.filter = `blur(${Math.round(load/3)}px)`;
 }
