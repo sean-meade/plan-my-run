d7a3e2c66d0889f93b7ed1760c413e59ad2b8593
Sean <seanpatmeade@gmail.com>
2022-01-22 22:30:13 +0000

    Alter bootstarp alert function to only add one alert.

--- assets/js/live-alert.js
+++ assets/js/live-alert.js
@@ -13,8 +13,8 @@
 function alert(message, type, alertPlaceholder) {
   var alertPlaceholder = document.getElementById(alertPlaceholder);
   var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-  alertPlaceholder.append(wrapper)
+  alertPlaceholder.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+  // alertPlaceholder.append(wrapper)
 }
 
 // if (alertTrigger) {
