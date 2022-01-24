db642aa4ce55e2af2ab4d75ddb55582948b4b83f
Sean <seanpatmeade@gmail.com>
2022-01-24 22:49:03 +0000

    change variable for both loops in weather-input to decern between the two. Add function that adds a day to the date and returns date.

--- assets/js/weather-input.js
+++ assets/js/weather-input.js
@@ -4,13 +4,13 @@ let min = 00,
     max = 23,
     selectTime = document.getElementById('weather-input-time');
 
-for (var i = min; i<=max; i++){
-    var opt = document.createElement('option');
-    opt.value = i;
-    if (i < 10) {
-      opt.innerHTML = "0" + i + ":00";
+for (let t = min; t <= max; t++){
+    let opt = document.createElement('option');
+    opt.value = t;
+    if (t < 10) {
+      opt.innerHTML = "0" + t + ":00";
     } else {
-      opt.innerHTML = i  + ":00";
+      opt.innerHTML = t + ":00";
     }
     
     selectTime.appendChild(opt);
@@ -26,16 +26,24 @@ let dayNames = [
   "Saturday"
 ]
 
+// https://stackoverflow.com/questions/563406/add-days-to-javascript-date
+// A function that adds a number of days to the given date and returns the date
+Date.prototype.addDays = function(days) {
+  let date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
 // https://www.delftstack.com/howto/javascript/javascript-get-week-number/#:~:text=The%20getDay()%20function%20returns,current%20week%20number%20is%20calculated.&text=Copy%20currentdate%20%3D%20new%20Date(),)%3B%20var%20numberOfDays%20%3D%20Math.
 
-let currentdate = new Date(),
+let currentdate = new Date()
     startDayOption = currentdate.getDay(),
     selectDay = document.getElementById('weather-input-day');
 
 console.log(currentdate.getMonth());
 
-for (var i = startDayOption; i<=startDayOption+3; i++){
-  var opt = document.createElement('option');
+for (let d = startDayOption; d <= startDayOption + 3; d++){
+  let opt = document.createElement('option');
   opt.value = i;
   opt.innerHTML = dayNames[i];
   selectDay.appendChild(opt);
