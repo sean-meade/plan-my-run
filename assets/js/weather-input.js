2c25de917c91adc2ae01427efdc6a552d7ff0cdf
Sean <seanpatmeade@gmail.com>
2022-01-24 22:28:00 +0000

    Using the same method for getting the days of the options in use on days of the week to change depending on what day it is.

--- assets/js/weather-input.js
+++ assets/js/weather-input.js
@@ -1,12 +1,37 @@
 
-
-var min = 00,
+// https://stackoverflow.com/questions/8674618/adding-options-to-select-with-javascript/8674667
+let min = 00,
     max = 23,
-    select = document.getElementById('weather-input-time');
+    selectTime = document.getElementById('weather-input-time');
 
 for (var i = min; i<=max; i++){
     var opt = document.createElement('option');
     opt.value = i;
     opt.innerHTML = i;
-    select.appendChild(opt);
+    selectTime.appendChild(opt);
+}
+
+let dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+]
+
+// https://www.delftstack.com/howto/javascript/javascript-get-week-number/#:~:text=The%20getDay()%20function%20returns,current%20week%20number%20is%20calculated.&text=Copy%20currentdate%20%3D%20new%20Date(),)%3B%20var%20numberOfDays%20%3D%20Math.
+
+let currentdate = new Date(),
+    startDayOption = currentdate.getDay(),
+    selectDay = document.getElementById('weather-input-day');
+
+console.log(startDayOption);
+
+for (var i = startDayOption; i<=startDayOption+3; i++){
+  var opt = document.createElement('option');
+  opt.value = i;
+  opt.innerHTML = dayNames[i];
+  selectDay.appendChild(opt);
 }
