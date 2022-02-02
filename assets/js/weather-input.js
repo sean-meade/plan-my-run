2c7d506dbd5bfc9ee86c0c1bb50c13c953f60e5d
Sean <seanpatmeade@gmail.com>
2022-02-02 15:14:54 +0000

    Remove the options within the selects in the html for weather output. format and comment weather-input.js

--- assets/js/weather-input.js
+++ assets/js/weather-input.js
@@ -1,5 +1,20 @@
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
-// https://stackoverflow.com/questions/8674618/adding-options-to-select-with-javascript/8674667
+let currentDate = new Date();
+let datesArray = [currentDate];
+
+let selectDay = document.getElementById('weather-input-day');
+
+// Following is adapted from https://stackoverflow.com/questions/8674618/adding-options-to-select-with-javascript/8674667 
+// where I auto fill the options for time
 let min = 0,
     max = 23,
     selectTime = document.getElementById('weather-input-time');
@@ -16,34 +31,29 @@ for (let t = min; t <= max; t++){
     selectTime.appendChild(opt);
 }
 
-let dayNames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+
 
 // https://stackoverflow.com/questions/563406/add-days-to-javascript-date
-// A function that adds a number of days to the given date and returns the date
+// 
+/**
+ * A function that takes in a number of days and adds it to the given date and returns that new date.
+ * @param {int} days number of days
+ * @returns 
+ */
 Date.prototype.addDays = function(days) {
   let date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
 };
 
-let currentdate = new Date();
-let datesArray = [currentdate];
+// Make an array using the above function of the dates of the next 4 days
 for (let dayNo = 1; dayNo < 4; dayNo++) {
-  datesArray.push(currentdate.addDays(dayNo));
+  datesArray.push(currentDate.addDays(dayNo));
 }
-    // startDayOption = currentdate.getDay(),
-let selectDay = document.getElementById('weather-input-day');
 
-// console.log(datesArray[1].getDate());
-
+// Create an option and add that date as the value and day from dayNames as the
+// innerHTML. This function keeps the options up to date as there are only 4 days 
+// of weather info
 for (let d = 0; d < 4; d++){
   let opt = document.createElement('option');
   // value is [dayNo, MonthNo]
